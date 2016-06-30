f4d4d18bc303e9e9ff755ed9d042921e83c138e2
fshaikh <fshaikh@cs.cmu.edu>
2016-06-30 17:26:15 +0000

    LW_FEATURE_LEVEL=auth causes rpcd to fail as tcp/ip rpc are not built. Removed it from default build file

--- debug/build.sh
+++ debug/build.sh
@@ -2,17 +2,14 @@
 
 export CWD=`pwd`
 
-export LW_FEATURE_LEVEL="auth"
-export LSA_RPC_SERVERS="yes"
-
-export CFLAGS="-w -Wno-error=unused-but-set-variable -Wno-error=implicit-function-declaration -Wno-error=sizeof-pointer-memaccess -Wno-error=unused-local-typedefs -Wno-error=pointer-sign -Wno-error=address"
+export CFLAGS="-g -w -Wno-error=unused-but-set-variable -Wno-error=implicit-function-declaration -Wno-error=sizeof-pointer-memaccess -Wno-error=unused-local-typedefs -Wno-error=pointer-sign -Wno-error=address"
 
 
 echo "Compiles on VMWARE photo 1.0 distro"
 echo "Compiles on VMWARE photo 1.0 distro"
 echo "Compiles on VMWARE photo 1.0 distro"
 
-#export DEBUG=--debug
+export DEBUG=--debug
 ../configure $DEBUG \
              --prefix=/opt/likewise \
              --libdir=/opt/likewise/lib64 \
@@ -20,7 +17,8 @@ echo "Compiles on VMWARE photo 1.0 distro"
              --datarootdir=/opt/likewise/share \
              --build-isas=x86_64 \
              --lw-bundled-libs='libedit' \
-             --enable-vmdir-provider=yes
+             --enable-vmdir-provider=yes \
+	     --package-deb=yes
 
 make all package
 
