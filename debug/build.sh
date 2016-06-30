54a52f30b5ab794615458fd9035e619cfaae40c3
fshaikh <fshaikh@cs.cmu.edu>
2016-06-30 05:29:31 +0000

    VMWware has released the same project as Likewise 6.2 This can be  downloaded from git clone https://github.com/vmware/likewise-open
    Taking this project to its logical conclusion,  made sure everything compiles on
    the VMWare photon OS.
    https://vmware.github.io/photon/ 1.0 iso
    
    To  compile
    install photon 1.0
    checkout source tree
    cd debug
    ./build.sh
    
    Thanks

---
+++ debug/build.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+export CWD=`pwd`
+
+export LW_BUILD_PHOTON=1
+export LW_FEATURE_LEVEL="auth"
+export LSA_RPC_SERVERS="yes"
+
+export CFLAGS="-w -Wno-error=unused-but-set-variable -Wno-error=implicit-function-declaration -Wno-error=sizeof-pointer-memaccess -Wno-error=unused-local-typedefs -Wno-error=pointer-sign -Wno-error=address"
+
+
+echo "Compiles on VMWARE photo 1.0 distro"
+echo "Compiles on VMWARE photo 1.0 distro"
+echo "Compiles on VMWARE photo 1.0 distro"
+
+#export DEBUG=--debug
+../configure $DEBUG \
+             --prefix=/opt/likewise \
+             --libdir=/opt/likewise/lib64 \
+             --datadir=/opt/likewise/share \
+             --datarootdir=/opt/likewise/share \
+             --build-isas=x86_64 \
+             --lw-bundled-libs='libedit' \
+             --enable-vmdir-provider=yes
+
+make all package
+
