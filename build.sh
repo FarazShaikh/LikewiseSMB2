787d0e81940426c8355c28cb6430b7e4aef323ed
faraz shaikh <fshaikh@cs.cmu.edu>
2016-07-21 14:20:27 -0700

    move build.sh out of ./debug folder. This allows a full rebuild by deleting the debug folder.

--- build.sh
+++ build.sh
@@ -3,11 +3,15 @@
 export CWD=`pwd`
 
 export CFLAGS="-g -w -Wno-error=unused-but-set-variable -Wno-error=implicit-function-declaration -Wno-error=sizeof-pointer-memaccess -Wno-error=unused-local-typedefs -Wno-error=pointer-sign -Wno-error=address"
+export GTK_CFLAGS=$CFLAGS
 
 
 echo "Compiles on VMWARE photo 1.0 distro"
 echo "Compiles on VMWARE photo 1.0 distro"
 echo "Compiles on VMWARE photo 1.0 distro"
+
+mkdir ./debug
+cd ./debug
 
 export DEBUG=--debug
 ../configure $DEBUG \
